d2212a3148ec278c4110bc8bac68cd5716f9019d
Nathan Hill <pjsstp@gmail.com>
2020-03-23 13:56:14 -0400

    building app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const db = require('./connection')
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
@@ -13,3 +14,4 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
