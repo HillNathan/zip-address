50b116309e755a3c2eaa1bb244df98b3a394ffe9
Nathan Hill <pjsstp@gmail.com>
2020-03-23 14:17:14 -0400

    building app

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const db = require('./connection')
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
