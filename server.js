0f4e1e0b06eb95e314dec4fd3916fd4fd46e28b2
Nathan Hill <pjsstp@gmail.com>
2020-03-23 12:23:08 -0400

    building

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// create a GET route
-require("./routes")(app);
+    app.use(express.static("client/build"));
+  }
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// app.get('/express_backend', (req, res) => {
-//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-// });
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
