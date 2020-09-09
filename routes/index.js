219b09523f0d7e6070c0a6ad85ca7e6c8c2f9809
Nathan Hill <pjsstp@gmail.com>
2020-09-09 12:05:43 -0400

    fixed merge conflicts and other cross-code

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// include any additional middleware declarations up here:
-// i.e.
-// const API = require("../controller");
-
-module.exports = app => {
-
-    // Starting route to confirm everything is set up to run through express
-=======
-const db = require("../controller");
-// var path = require("path");
 
 module.exports = app => {
 
@@ -16,28 +5,8 @@ module.exports = app => {
     //     res.sendFile('path.join("index.html")');
     // })
 
->>>>>>> 50b116309e755a3c2eaa1bb244df98b3a394ffe9
     app.get('/express_backend', (req, res) => {
         res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
       });
 
-<<<<<<< HEAD
 }
-
-=======
-    app.get('/get_home', (req, res) => {
-        db.getInfo(17603, response => {
-            res.send({data: response})
-            }
-        )
-    });
-
-    app.post('/get_zip', (req, res) => {
-        db.getInfo(req.body.zipCode, response => {
-            res.send({data: response})
-            }
-        )    
-    });
-
-}
->>>>>>> 50b116309e755a3c2eaa1bb244df98b3a394ffe9
